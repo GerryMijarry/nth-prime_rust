f7092ce2ee519fa4d0095347cdb0374c51853b5b
Gerard Mijares <gerardmijares39@gmail.com>
2021-09-16 15:40:55 +0400

    setup excercise with failing tests

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod nth_prime;
+
+fn main() {
+    let x = nth_prime::nth(32);
+    println!("{}", x);
+}
---
+++ src/nth_prime.rs
@@ -0,0 +1,25 @@
+pub fn nth(n: u32) -> u32 {
+        n + 1
+    }
+}
+
+
+#[test]
+fn test_first_prime() {
+    assert_eq!(nth(0), 2);
+}
+
+#[test]
+fn test_second_prime() {
+    assert_eq!(nth(1), 3);
+}
+
+#[test]
+fn test_sixth_prime() {
+    assert_eq!(nth(5), 13);
+}
+
+#[test]
+fn test_big_prime() {
+    assert_eq!(nth(10_000), 104_743);
+}
