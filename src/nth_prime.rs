9b314c5f010a0cf661d247c286ae94316580c4ce
Gerard Mijares <gerardmijares39@gmail.com>
2021-09-21 18:42:42 +0400

    solved coding challenge

--- src/nth_prime.rs
+++ src/nth_prime.rs
@@ -1,7 +1,31 @@
 pub fn nth(n: u32) -> u32 {
-        n + 1
-    }
+    primes().nth(n as usize).unwrap()
+}
 
+pub struct Primes {
+    primes: Vec<u32>,
+}
+
+pub fn primes() -> Primes {
+    Primes { primes: Vec::new() }
+}
+
+impl Iterator for Primes {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut candidate = self.primes.last().copied().unwrap_or(1);
+        loop {
+            candidate += 1 + (candidate > 2) as Self::Item;
+            if self.primes.iter()
+                .take_while(|&p| p * p <= candidate)
+                .all(|&p| candidate % p != 0)
+            {
+                self.primes.push(candidate);
+                return Some(candidate);
+            }
+        }
+    }
+}
 
 #[test]
 fn test_first_prime() {
